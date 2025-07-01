fc70476d4a6cff48ffc90211001efd7d7d481820
imdavyking <yungdynamic53@gmail.com>
2025-07-01 21:03:46 +0100

    chore: updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import NavHeader from "./components/NavHeader";
-import { WalletProvider } from "./context/WalletContext";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -8,13 +7,11 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
-      <WalletProvider>
-        <ToastContainer />
-        <BrowserRouter>
-          <NavHeader />
-          <Router />
-        </BrowserRouter>
-      </WalletProvider>
+      <ToastContainer />
+      <BrowserRouter>
+        <NavHeader />
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
