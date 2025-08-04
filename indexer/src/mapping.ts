cd32ccf959abf04ceed4c24e6b7bc6dc595f0043
imdavyking <yungdynamic53@gmail.com>
2025-08-04 01:20:37 +0100

    chore: updated

--- indexer/src/mapping.ts
+++ indexer/src/mapping.ts
@@ -1,107 +1,40 @@
-import {
-  ethereum, BigInt
-} from '@graphprotocol/graph-ts'
+import { ethereum, BigInt } from "@graphprotocol/graph-ts";
+import { LenderLiquidityUpdated as LenderLiquidityUpdatedEvent } from "../generated/credlink/credlink";
+import { LenderLiquidityUpdated } from "../generated/schema";
 
-import {
-  EventToken as EventTokenEvent,
-  Transfer   as TransferEvent,
-} from '../generated/Poap/Poap'
-
-import {
-  Token,
-  Account,
-  Event,
-  Transfer,
-} from '../generated/schema'
-
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-
-
-function createEventID(event: ethereum.Event): string
-{
-  return event.block.number.toString().concat('-').concat(event.logIndex.toString());
+function createEventID(lender: string, token: string): string {
+  return lender
+    .toLowerCase()
+    .concat("-")
+    .concat(token.toLowerCase());
 }
 
-export function handleEventToken(ev: EventTokenEvent): void
-{
-  let event = Event.load(ev.params.eventId.toString());
-  // This handler always run after the transfer handler
-  let token = Token.load(ev.params.tokenId.toString())!;
-  if (event == null) {
-    event               = new Event(ev.params.eventId.toString());
-    event.tokenCount    = BigInt.fromI32(0);
-    event.tokenMints    = BigInt.fromI32(0);
-    event.transferCount = BigInt.fromI32(0);
-    event.created       = ev.block.timestamp
-  }
-
-  event.tokenCount    += BigInt.fromI32(1);
-  event.tokenMints    += BigInt.fromI32(1);
-  event.transferCount += BigInt.fromI32(1);
-  token.event         = event.id;
-  token.mintOrder     = event.tokenMints;
-  event.save();
-  token.save();
+function createEventIDByBlock(event: ethereum.Event): string {
+  return event.block.number
+    .toString()
+    .concat("-")
+    .concat(event.logIndex.toString());
 }
 
-export function handleTransfer(ev: TransferEvent): void {
-  let token    = Token.load(ev.params.tokenId.toString());
-  let from     = Account.load(ev.params.from.toHex());
-  let to       = Account.load(ev.params.to.toHex());
-  let transfer = new Transfer(createEventID(ev));
+export function handleLenderLiquidityUpdate(
+  ev: LenderLiquidityUpdatedEvent
+): void {
+  let lender = ev.params.lender.toHexString();
+  let token = ev.params.token.toHexString();
 
-  if (from == null) {
-    from              = new Account(ev.params.from.toHex());
-    // The from account at least has to own one token
-    from.tokensOwned  = BigInt.fromI32(1);
-  }
-  // Don't subtracts from the ZERO_ADDRESS (it's the one that mint the token)
-  // Avoid negative values
-  if(from.id != ZERO_ADDRESS) {
-    from.tokensOwned -= BigInt.fromI32(1);
-  }
-  from.save();
+  let id = createEventID(lender, token);
+  let entity = LenderLiquidityUpdated.load(id);
 
-  if (to == null) {
-    to              = new Account(ev.params.to.toHex());
-    to.tokensOwned  = BigInt.fromI32(0);
-  }
-  to.tokensOwned += BigInt.fromI32(1);
-  to.save();
-
-  if (token == null) {
-    token               = new Token(ev.params.tokenId.toString());
-    token.transferCount = BigInt.fromI32(0);
-    token.created       = ev.block.timestamp
-  }
-  token.owner = to.id;
-  token.transferCount += BigInt.fromI32(1);
-  token.save();
-
-
-  if (token.event != null) {
-    let event = Event.load(token.event as string);
-
-    if(event != null) {
-      // Add one transfer
-      event.transferCount += BigInt.fromI32(1);
-  
-      // Burning the token
-      if(to.id == ZERO_ADDRESS) {
-        event.tokenCount    -= BigInt.fromI32(1);
-        // Subtract all the transfers from the burned token
-        event.transferCount -= token.transferCount;
-      }
-      event.save();
-    }
+  if (entity == null) {
+    entity = new LenderLiquidityUpdated(id);
   }
 
-  
+  entity.lender = ev.params.lender;
+  entity.token = ev.params.token;
+  entity.availableAmount = ev.params.availableAmount;
+  entity.blockNumber = ev.block.number;
+  entity.transactionHash = ev.transaction.hash;
+  entity.timestamp = ev.block.timestamp;
 
-  transfer.token       = token.id;
-  transfer.from        = from.id;
-  transfer.to          = to.id;
-  transfer.transaction = ev.transaction.hash;
-  transfer.timestamp   = ev.block.timestamp;
-  transfer.save();
+  entity.save();
 }
